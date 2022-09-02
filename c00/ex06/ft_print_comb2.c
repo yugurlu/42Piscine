e27bef7368f8f5d8ebe7b9531f6d03789ab3d9a2
Yusuf Ugurlu <108735272+yusufugurlu42@users.noreply.github.com>
2022-09-02 10:16:23 +0300

    C00 - %85

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yugurlu <yugurlu@student.42istanbul.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 11:07:13 by yugurlu           #+#    #+#             */
+/*   Updated: 2022/08/27 21:44:52 by yugurlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;
+
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		ft_putchar(a / 10 + '0');
+		ft_putchar(a % 10 + '0');
+		ft_putchar(' ');
+		ft_putchar(b / 10 + '0');
+		ft_putchar(b % 10 + '0');
+		if (a < 98)
+			write(1, ", ", 2);
+		if (++b > 99)
+			b = ++a + 1;
+	}
+}
